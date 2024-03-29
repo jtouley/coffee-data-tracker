581228145d0ed444bab72c441733a41f5d3b1a90
jtouley <jtouley@gmail.com>
2024-03-29 11:16:33 -0400

    updating cloud function src path

--- src/terraform/gcs_buckets.tf
+++ src/terraform/gcs_buckets.tf
@@ -9,7 +9,7 @@ resource "google_storage_bucket" "coffee_data_bucket" {
 resource "google_storage_bucket_object" "function_source" {
   name   = "coffee-data-function.zip"
   bucket = google_storage_bucket.cloud_functions_bucket.name
-  source = "cloud_function/coffee-data-function.zip"
+  source = "src/cloud_function/coffee-data-function.zip"
 }
 
 resource "random_id" "bucket_suffix" {
