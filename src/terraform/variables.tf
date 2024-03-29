075b59418cce05e8cc2c5eb254ba8210ae5428c2
jtouley <jtouley@gmail.com>
2024-03-29 10:58:33 -0400

    modifying the rest of variables.tf to remove duplicates

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -1,38 +1,13 @@
-variable "project_id" {
-  description = "The ID of the Google Cloud project"
-  type        = string
-}
-
 variable "region" {
   description = "The Google Cloud region where resources will be deployed"
   type        = string
   default     = "us-central1"
 }
 
-variable "function_name" {
-  description = "The name of the Google Cloud Function"
-  type        = string
-}
-
 variable "runtime" {
   description = "The runtime environment for the Google Cloud Function"
   type        = string
   default     = "python39"
-}
-
-variable "entry_point" {
-  description = "The name of the function (within the code) that will be executed"
-  type        = string
-}
-
-variable "source_archive_bucket" {
-  description = "The name of the Google Cloud Storage bucket where the Cloud Function source code will be stored"
-  type        = string
-}
-
-variable "source_archive_object" {
-  description = "The name of the Cloud Function source code archive file (ZIP format) stored in the Google Cloud Storage bucket"
-  type        = string
 }
 
 variable "set_trigger_http" {
